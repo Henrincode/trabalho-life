080beb7de13549369ca7dfb2d6fcee2103acccf7
Henrincode <spellctro@gmail.com>
2025-09-20 15:44:07 -0300

    build: criei uma base de dados para preencher os quadros ao abrir o site

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // seletores
 
 const formal = document.querySelector(".formal section")
+const naoFormal = document.querySelector(".nao-formal section")
+const informal = document.querySelector(".informal section")
 
 const formCriar = document.querySelectorAll(".criar-nota")
+
+let dbNotas
+
+preencherNotas()
 
 formCriar.forEach(form => {
     form.addEventListener("submit", e => {
@@ -11,18 +17,14 @@ formCriar.forEach(form => {
         const texto = form.nota.value
         form.nota.value = ''
 
-        if(texto == '') return
+        if (texto == '') return
 
         const pai = form.closest('.categoria')
         const section = pai.querySelector('section')
 
         const nota = criarNota(texto)
 
-        nota.addEventListener('click', e => {
-            e.currentTarget.remove()
-        })
-
-        section.appendChild(nota)    
+        section.appendChild(nota)
     })
 })
 
@@ -31,5 +33,31 @@ function criarNota(texto) {
     const nota = document.createElement('div')
     nota.classList.add('nota')
     nota.textContent = texto
+    nota.addEventListener('click', e => {
+        e.currentTarget.remove()
+    })
     return nota
+}
+
+async function preencherNotas(params) {
+    const data = await fetch('data-notas.json')
+    const notas = await data.json()
+
+    dbNotas = notas
+
+    notas.forEach(nota => {
+
+        if (nota.categoria == 1) {
+            const nnota = criarNota(nota.texto)
+            formal.appendChild(nnota)
+        }
+        if (nota.categoria == 2) {
+            const nnota = criarNota(nota.texto)
+            naoFormal.appendChild(nnota)
+        }
+        if (nota.categoria == 1) {
+            const nnota = criarNota(nota.texto)
+            informal.appendChild(nnota)
+        }
+    })
 }
